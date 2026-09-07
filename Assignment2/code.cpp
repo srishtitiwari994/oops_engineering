a329f6bd0bc2bd6848b5cec4f0df25e3951215a4
srishtitiwari994 <srishtitiwari994@gmail.com>
2026-09-07 10:29:59 +0530

    Correct even number summation logic
    Fix the loop to correctly sum even numbers from 2 to 100.

--- Assignment2/code.cpp
+++ Assignment2/code.cpp
@@ -1,1 +1,18 @@
+#include <iostream> using namespace std;
+
+int main()
+{
+int sum = 0;
+
+// Add even numbers from 2 to 100 for (int i = 2; i <= 100; i += 2)
+{
+sum += i;
+}
+
+// Display the final sum
+cout << "Sum of the first 50 even numbers = "
+<< sum << endl;
+
+return 0;
+}
 
